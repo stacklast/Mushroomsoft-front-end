ee181b3bc902697148eb66fb49f7ea32f692c6f9
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-05-20 11:05:20 -0500

    ADD: Estructure for the project, Lazy Loading Routes and Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { 
+    path: 'home', 
+    redirectTo: '',  
+    pathMatch: 'full' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
